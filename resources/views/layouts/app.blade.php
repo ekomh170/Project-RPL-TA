8a8ce333a57c4b26786a0b7bdf2b216f3fec9d2f
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2025-01-04 01:46:13 +0700

    Mengubah Favicon & Menyesuaikan Halaman Utama

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('admin/dist/img/logo/handygo.png') }}" type="image/x-icon">
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
